6fc0e097b63518227bc4e6d71989ea3006781f5a
golfstream83 <victul83@gmail.com>
2016-06-20 23:14:45 +0300

    add task factorial

---
+++ factorial/Factorial.java
@@ -0,0 +1,33 @@
+package JavaCourse.Modul_1.Task_4_2;
+
+public class Factorial
+{
+    private int value;
+
+    public Factorial(int value)
+    {
+        this.value = value;
+    }
+
+    public static void main(String[] args)
+    {
+        Factorial factorial = new Factorial(Integer.valueOf(args[0]));
+        System.out.println(String.format("Factorial of a given number is %s",factorial.calculation()));
+    }
+
+    public int calculation(){
+        int result = 1;
+        if (this.value == 0) {
+            return result;
+        }
+        else
+        {
+            while (this.value != 0)
+            {
+                result *= this.value;
+                this.value--;
+            }
+            return result;
+        }
+    }
+}
