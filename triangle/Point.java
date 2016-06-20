8d7acaecd06bafcbad2a2c8e4a3b0458f7d88a1b
golfstream83 <victul83@gmail.com>
2016-06-20 11:54:07 +0300

    test

---
+++ triangle/Point.java
@@ -0,0 +1,19 @@
+package triangle;
+
+import java.lang.Math;
+
+public class Point
+{
+    public double x;
+    public double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distanceTo(Point point) {
+        //calculate distance between two points
+        return Math.sqrt(Math.pow(point.y - this.y, 2) + Math.pow(point.x - this.x, 2));
+    }
+}
