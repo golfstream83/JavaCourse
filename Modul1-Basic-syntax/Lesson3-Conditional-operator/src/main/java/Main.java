0c119be62a3482ef31621a5bd7eeb51d52d4fc01
golfstream83 <victul83@gmail.com>
2016-07-11 23:49:36 +0300

    Add files via upload

---
+++ Modul1-Basic-syntax/Lesson3-Conditional-operator/src/main/java/Main.java
@@ -0,0 +1,23 @@
+
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Point a = new Point(1, 1);
+        Point b = new Point(3, 3);
+        Point c = new Point(-20, 2);
+        Triangle triangle = new Triangle(a, b, c);
+        if ((triangle.getLenAB() + triangle.getLenBC()) > triangle.getLenCA() &&
+                (triangle.getLenBC() + triangle.getLenCA()) > triangle.getLenAB() &&
+                (triangle.getLenCA() + triangle.getLenAB()) > triangle.getLenBC())
+        {
+            System.out.println("Triangle area: " + triangle.area() + "\nThe length of the logest side: " +
+                    triangle.maxSideTriangle());
+
+        }
+        else {
+            System.out.println("A triangle can't be built.");
+        }
+    }
+}
