31daaeed82187a18552e2fb472e8b99c83694692
golfstream83 <victul83@gmail.com>
2016-07-15 10:11:46 +0300

    ver 1.1

--- Modul1-Basic-syntax/Lesson5-Arrays/src/main/java/BubbleSortArray.java
+++ Modul1-Basic-syntax/Lesson5-Arrays/src/main/java/BubbleSortArray.java
@@ -1,19 +1,19 @@
 public class BubbleSortArray {
 
     public static void main(String[] args) {
-        int values[] = {3, 45, 6, 2, 11, 1};
-        for (int x : sort(values)) {
+        int values[] = {3, 45, 6, 2, 11, 1, 56, 5};
+        for (int x : new BubbleSortArray().sort(values)) {
             System.out.println(x);
         }
     }
 
-    static int[] sort(int array[]){
-        for (int i = 0; i < array.length; i++){
+    int[] sort(int array[]){
+        for (int i = 0; i < array.length - 1; i++){
             for (int j = 0; j < array.length - 1; j++){
-                if (array[i] < array[j]){
-                    int temp = array[i];
-                    array[i] = array [j];
-                    array[j] = temp;
+                if (array[j] > array[j + 1]){
+                    int temp = array[j];
+                    array[j] = array [j + 1];
+                    array[j + 1] = temp;
                 }
             }
         }
