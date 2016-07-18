7f21fd7371b9196ce2b7e67ef0db0dfd11ffca7e
golfstream83 <victul83@gmail.com>
2016-07-18 23:34:30 +0300

    ver 1

--- Modul1-Basic-syntax/Lesson5-Arrays/src/main/java/RemovingDuplicatesInTheArray.java
+++ Modul1-Basic-syntax/Lesson5-Arrays/src/main/java/RemovingDuplicatesInTheArray.java
@@ -3,13 +3,30 @@ public class RemovingDuplicatesInTheArray {
 
     public static void main(String[] args) {
         RemovingDuplicatesInTheArray removingDuplicatesInTheArray = new RemovingDuplicatesInTheArray();
-        String[] suppliedArray = new String[] {"one", "four", "two", "three", "four", "one"};
+        String[] suppliedArray = new String[] {"one", "one", "two", "three", "four", "four"};
         removingDuplicatesInTheArray.removingDuplicates(suppliedArray);
         removingDuplicatesInTheArray.print(suppliedArray);
     }
 
     String[] removingDuplicates(String[] array) {
-
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j].equals(array[i])) {
+                    array[j] = null;
+                }
+            }
+        }
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] == null) {
+                for (int j = i + 1; j < array.length; j++) {
+                    if (array[j] != null) {
+                        array[i] = array[j];
+                        array[j] = null;
+                        break;
+                    }
+                }
+            }
+        }
         return array;
     }
 
