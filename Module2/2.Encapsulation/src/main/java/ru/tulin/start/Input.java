62634e79f956a445be25680bebb0f854e29d8340
golfstream83 <victul83@gmail.com>
2016-10-24 23:27:42 +0300

    ver 2 //fixed javadoc

--- Module2/2.Encapsulation/src/main/java/ru/tulin/start/Input.java
+++ Module2/2.Encapsulation/src/main/java/ru/tulin/start/Input.java
@@ -7,8 +7,19 @@ package ru.tulin.start;
  */
 
 public interface Input {
-
+    /**
+     * method reads the data entered by the user
+     * @param question
+     * @return
+     */
     String ask(String question);
 
+    /**
+     * method checks the entered key
+     * @param question
+     * @param range
+     * @return
+     * @throws MenuOutException
+     */
     int ask(String question, int[] range) throws MenuOutException;
 }
